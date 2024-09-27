362accb90423844caa22680e60c668efc31acea3
Guedo <g.guedes.a@hotmail.com>
2024-09-26 23:28:51 -0300

    feat: add carousel autoplay

--- app/_components/desktop/barbershop-carousel.tsx
+++ app/_components/desktop/barbershop-carousel.tsx
@@ -1,4 +1,6 @@
-import { Barbershop } from "@prisma/client"
+"use client"
+
+import Autoplay from "embla-carousel-autoplay"
 import {
   Carousel,
   CarouselContent,
@@ -10,31 +12,62 @@ import BarbershopItem from "./barbershop-item"
 
 interface BarbershopCarouselProps {
   title: string
-  barbershops: Barbershop[]
+  barbershops: {
+    id: string
+    name: string
+    address: string
+    imageUrl: string
+  }[]
+  delay?: number
 }
 
 const BarbershopCarousel = ({
   title,
   barbershops,
+  delay,
 }: BarbershopCarouselProps) => {
   return (
     <>
       <h2 className="mb-5 text-sm font-bold uppercase text-gray-400">
         {title}
       </h2>
-      <Carousel opts={{ align: "start" }} className="w-full">
-        <CarouselContent>
-          {barbershops.map((barbershop) => (
-            <div key={barbershop.id} className="gap-3">
-              <CarouselItem className="md:basis-full lg:basis-1/2 xl:basis-1/3">
-                <BarbershopItem barbershop={barbershop} />
-              </CarouselItem>
-            </div>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
+      {delay ? (
+        <Carousel
+          opts={{ align: "start" }}
+          className="w-full"
+          plugins={[
+            Autoplay({
+              delay: delay,
+            }),
+          ]}
+        >
+          <CarouselContent>
+            {barbershops.map((barbershop) => (
+              <div key={barbershop.id} className="gap-3">
+                <CarouselItem className="md:basis-full lg:basis-1/2 xl:basis-1/3">
+                  <BarbershopItem barbershop={barbershop} />
+                </CarouselItem>
+              </div>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      ) : (
+        <Carousel opts={{ align: "start" }} className="w-full">
+          <CarouselContent>
+            {barbershops.map((barbershop) => (
+              <div key={barbershop.id} className="gap-3">
+                <CarouselItem className="md:basis-full lg:basis-1/2 xl:basis-1/3">
+                  <BarbershopItem barbershop={barbershop} />
+                </CarouselItem>
+              </div>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      )}
     </>
   )
 }
