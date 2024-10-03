73b1f58f2012b999eb6596439dd0b8fd5e1d9bf5
Gabriel Guedes Ambrogi <g.guedes.a@hotmail.com>
2024-10-03 17:57:04 -0300

    fix: create verification before booking

--- app/_actions/create-booking.ts
+++ app/_actions/create-booking.ts
@@ -12,16 +12,35 @@ interface CreateBookingParams {
 
 export const createBooking = async (params: CreateBookingParams) => {
   const session = await getServerSession(authOptions)
+
   if (!session || !session.user) {
     throw new Error("Usuário não autenticado")
   }
+
+  const userId = (session.user as any).id
+
+  const existingBooking = await db.booking.findFirst({
+    where: {
+      serviceId: params.serviceId,
+      date: params.date,
+      userId: userId,
+    },
+  })
+
+  if (existingBooking) {
+    throw new Error(
+      "Reserva duplicada: já existe uma reserva para esse serviço e horário.",
+    )
+  }
+
   const bookingData = {
     ...params,
-    userId: (session.user as any).id,
+    userId: userId,
   }
 
   await db.booking.create({
     data: bookingData,
   })
+
   revalidatePath("/barbershops/[id]")
 }
